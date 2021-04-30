eab93c112c02443eec16f0962ab27ed55559b235
atorasuto <82609933+atorasuto@users.noreply.github.com>
2021-04-30 18:23:13 +0900

    Update add.php

--- add.php
+++ add.php
@@ -3,12 +3,12 @@
 header("Content-type: text/html; charset=utf-8");
  
 //データベース接続
-$server = "localhost";  
-$userName = "root"; 
-$password = ""; 
-$dbName = "kadai";
+$server = "sv5.php.xdomain.ne.jp";  
+$userName = "acrovision_002"; 
+$password = "AcroXpass2021"; 
+$dbName = "acrovision_002";
  
-$mysqli = new mysqli($server, $userName, $password,$dbName);
+$mysqli = new mysqli($server, $userName, $password, $dbName);
  
 if ($mysqli->connect_error){
 	echo $mysqli->connect_error;
